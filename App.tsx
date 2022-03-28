dc70b590fdea0453ae67252fdccca225493e7cb1
Leonardo <leonardo.pacciulli@opah.com.br>
2022-03-28 12:58:22 -0300

    Created the Scheduling completed screen

--- App.tsx
+++ App.tsx
@@ -7,9 +7,7 @@ import theme from './src/styles/theme';
 import { ThemeProvider } from 'styled-components'
 
 import Home from './src/screens/Home';
-import CarDetails from './src/screens/CarDetails';
-import Scheduling from './src/screens/Scheduling';
-import SchedulingDetails from './src/screens/SchedulingDetails';
+import SchedulingComplete from './src/screens/SchedulingComplete';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +24,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
