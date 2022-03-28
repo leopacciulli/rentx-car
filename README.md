2ef8139f7c79ee0b77767208566172a457f641e5
Leonardo <leonardo.pacciulli@opah.com.br>
2022-03-28 14:20:10 -0300

    Update README.md

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-<h3 align="center">
+<h1 align="center">
 🚧 In Development 🚧 
 <br>
 An app with React Native using Expo
+</h1>
+
+<br>
+<br>
+
+<h3 align="center">
+  <img src="./src/assets/logo.svg" width="250px">
 </h3>
 
 <br>
@@ -10,6 +17,19 @@ An app with React Native using Expo
 ### 📝 About the project
 RentX is a car rental app. On the home page, the user will be able to see the list of cars available to rent, after selecting the desired car, they will go to the car page, where they will be able to see more information and select the period in which they would like to have the car rented. Then you will confirm the information and confirm the rental.
 On the main page there will be a floating button where the user will be able to see the list of cars that are currently being rented.
+
+<br>
+
+### 📱 Mobile
+The Mobile was being developed with React Native and TypeScript - with Expo. Styled-components. The navigation screens were made with React Navigation (stack).
+<br>
+For now, the app is using Mocks to show the cars.
+
+<br>
+
+<h3 align="center">
+  <img src="./src/assets/home.png" width="300px">
+</h3>
 
 <br>
 <br>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
@@ -16,16 +16,41 @@ import {
 
 const Home = () => {
   const navigation = useNavigation();
+  const [cars, setCars] = useState([]);
 
-  const carData = {
-    brand: 'Audi',
-    name: 'RS 5 Coupe',
-    rent: {
-      period: 'Ao dia',
-      price: 120
-    },
-    thumbnail: 'https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/7/7d/2018_Audi_S5_TFSi_Quattro_Automatic_3.0_Front.jpg/1200px-2018_Audi_S5_TFSi_Quattro_Automatic_3.0_Front.jpg'
-  }
+  useEffect(() => {
+    const dataCars = [
+      {
+        brand: 'Audi',
+        name: 'RS 5 Coupe',
+        rent: {
+          period: 'Ao dia',
+          price: 4899
+        },
+        thumbnail: 'https://www.autoo.com.br/fotos/2020/4/1280_960/audi_rs-5-coupe_2021_1_09042020_29446_1280_960.jpg'
+      },
+      {
+        brand: 'Audi',
+        name: 'R8 V10 Plus',
+        rent: {
+          period: 'Ao dia',
+          price: 1299
+        },
+        thumbnail: 'https://3.bp.blogspot.com/-K_JRNTns-54/WSVsggVcBWI/AAAAAAACk-0/PJFLtLEfQZAHZhBZT4PMSCOBuILMxvh-QCLcB/s1600/Audi-R8-2018-Brasil%2B%25281%2529.jpg'
+      },
+      {
+        brand: 'Audi',
+        name: 'A3 Sedan',
+        rent: {
+          period: 'Ao dia',
+          price: 5995
+        },
+        thumbnail: 'https://s2.glbimg.com/klCdUmklo_9o226YG4-SnFg2l4s=/0x0:620x413/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/P/2/ynYtR7SI2uYcrtx0NiqA/2019-04-25-r8coupe.jpg'
+      }
+    ]
+
+    setCars(dataCars)
+  }, [])
 
   const handleCarDetails = () => {
     navigation.navigate('CarDetails')
@@ -47,15 +72,15 @@ const Home = () => {
           />
 
           <TotalCars>
-            Total de 12 carros
+            Total de 3 carros
           </TotalCars>
         </HeaderContent>
       </Header>
 
       <CarList 
-        data={[1,2,3]}
+        data={cars}
         keyExtractor={item => String(item)}
-        renderItem={({ item }) => <Car data={carData} onPress={handleCarDetails} />}
+        renderItem={({ item }) => <Car data={item} onPress={handleCarDetails} />}
       /> 
     </Container>
   )
