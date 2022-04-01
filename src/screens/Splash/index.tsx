54c71e6bebfae0e50a1c34046892a3ab2184ea38
Leonardo <leonardo.pacciulli@opah.com.br>
2022-04-01 11:41:01 -0300

    Creating my own Splash Screen

---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Animated from 'react-native-reanimated';
+
+import {
+  Container
+} from './styles';
+
+const Splash = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default Splash
